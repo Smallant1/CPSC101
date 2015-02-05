cc935b5da9c1a3a5a4ff456c27fd4e573d9c8a8a
Smallant1 <tlair@outlook.com>
2015-02-04 19:22:37 -0800

    Update MainPanel.java

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,15 +1,33 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
 
 
 public class MainPanel extends JPanel
 {
 
+	JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+	JSplitPane editSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+	JEditorPane edit = new JEditorPane();
+	JEditorPane file = new JEditorPane();
+	JEditorPane error = new JEditorPane();
+	
 	public MainPanel()
 	{
-		//code to add components to the panel
-		//this.add(new JSplitPane);
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.WHITE);
+	
+		editSplitPane.add(edit);
+		editSplitPane.add(error);
 		
-		
+		mainSplitPane.setOneTouchExpandable(true);
+		mainSplitPane.add(file);
+		mainSplitPane.add(editSplitPane);
+		this.add(mainSplitPane);
 	}
 	
 }
